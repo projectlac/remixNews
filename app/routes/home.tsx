82d1f16dcdb9bc13f0ea3e148de6262d691a3a5b
Harry Nguyen <harry.nguyen2@techvify.com.vn>
2022-08-25 14:49:37 +0700

    remix config

---
+++ app/routes/home.tsx
@@ -0,0 +1,27 @@
+// app/routes/home.tsx
+
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+
+import { Layout } from "~/components/layout";
+import { UserPanel } from "~/components/user-panel";
+import { requireUserId } from "~/utils/auth.server";
+import { getOtherUsers } from "~/utils/users.server";
+import { useLoaderData } from "@remix-run/react";
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+  const users = await getOtherUsers(userId);
+  return json({ users });
+};
+
+export default function Home() {
+  const { users } = useLoaderData();
+  return (
+    <Layout>
+      <div className="h-full flex">
+        <UserPanel users={users} />
+        <div className="flex-1"></div>
+      </div>
+    </Layout>
+  );
+}
